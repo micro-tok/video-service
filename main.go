aa69dac93f3c3a81ef6fcb0ba59c166312e49196
Martin Bozhilov <martin@bozhilov.me>
2024-04-05 23:19:57 +0300

    Feature/grpc (#6)
    * [ADD] proto
    
    * [ADD] grpc server
    
    * [FIX] update go.mod
    
    * [FIX] merge

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
+import (
+	"net"
+
+	"github.com/micro-tok/video-service/pkg/config"
+	"google.golang.org/grpc"
+)
+
 func main() {
-	// cfg := config.LoadConfig()
+	cfg := config.LoadConfig()
 
 	//TODO: init cassandra conn
 
+	lis, err := net.Listen("tcp", "localhost:"+cfg.ServicePort)
+	if err != nil {
+		panic(err)
+	}
+
+	grpcServer := grpc.NewServer()
+
 	//TODO: deploy grpcServer
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
 
 }
