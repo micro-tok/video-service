3113c71bb9b78afec650572c72ff7f834486b67e
Martin Bozhilov <marogo142005@gmail.com>
2024-04-06 00:14:50 +0300

    [ADD] Cassandra config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,17 +3,21 @@ package config
 import "os"
 
 type Config struct {
-	BucketKey    string
-	BucketSecret string
-	BucketName   string
-	ServicePort  string
+	BucketKey          string
+	BucketSecret       string
+	BucketName         string
+	ServicePort        string
+	CassandraClusterIP string
+	CassandraKeyspace  string
 }
 
 func LoadConfig() *Config {
 	return &Config{
-		BucketKey:    os.Getenv("BUCKET_KEY"),
-		BucketSecret: os.Getenv("BUCKET_SECRET"),
-		BucketName:   os.Getenv("BUCKET_NAME"),
-		ServicePort:  os.Getenv("SERVICE_PORT"),
+		BucketKey:          os.Getenv("BUCKET_KEY"),
+		BucketSecret:       os.Getenv("BUCKET_SECRET"),
+		BucketName:         os.Getenv("BUCKET_NAME"),
+		ServicePort:        os.Getenv("SERVICE_PORT"),
+		CassandraClusterIP: os.Getenv("CASSANDRA_CLUSTER_IP"),
+		CassandraKeyspace:  os.Getenv("CASSANDRA_KEYSPACE"),
 	}
 }
