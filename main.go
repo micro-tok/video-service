a1d7dab1e97a25961dd8e794400b649eff5c4de5
Martin Bozhilov <marogo142005@gmail.com>
2024-04-05 22:37:16 +0300

    [ADD] grpc server

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"net"
+
 	"github.com/micro-tok/video-service/pkg/config"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -9,6 +12,17 @@ func main() {
 
 	//TODO: init cassandra conn
 
+	lis, err := net.Listen("tcp", "localhost:"+cfg.ServicePort)
+	if err != nil {
+		panic(err)
+	}
+
+	grpcServer := grpc.NewServer()
+
 	//TODO: deploy grpcServer
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
 
 }
