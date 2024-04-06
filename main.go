7a32c36b12d799918d7652b77df1513bfb2628df
Martin Bozhilov <marogo142005@gmail.com>
2024-04-06 21:53:09 +0300

    [ADD] register service for grpc

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"net"
 
+	"github.com/micro-tok/video-service/pkg/cassandra"
 	"github.com/micro-tok/video-service/pkg/config"
+	"github.com/micro-tok/video-service/pkg/pb"
+	"github.com/micro-tok/video-service/pkg/s3"
+	"github.com/micro-tok/video-service/pkg/services"
 	"google.golang.org/grpc"
 )
 
 func main() {
 	cfg := config.LoadConfig()
 
-	//TODO: init cassandra conn
+	cassService := cassandra.NewCassandraService(cfg)
+
+	s3Service := s3.NewAWSService(cfg)
 
 	lis, err := net.Listen("tcp", "localhost:"+cfg.ServicePort)
 	if err != nil {
@@ -19,7 +25,8 @@ func main() {
 
 	grpcServer := grpc.NewServer()
 
-	//TODO: deploy grpcServer
+	// Register the service
+	pb.RegisterVideoServiceServer(grpcServer, services.NewVideoService(cassService, s3Service))
 
 	if err := grpcServer.Serve(lis); err != nil {
 		panic(err)
