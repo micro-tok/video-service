922740a37fa7cf230370ac0297e9c2c8b5d734bd
Martin Bozhilov <marogo142005@gmail.com>
2024-04-07 11:46:38 +0300

    [ADD] Redis to service

--- go.mod
+++ go.mod
@@ -4,13 +4,17 @@ go 1.22.1
 
 require (
 	github.com/aws/aws-sdk-go v1.51.16
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/gocql/gocql v1.6.0
 	github.com/gofrs/uuid/v5 v5.0.0
+	github.com/joho/godotenv v1.5.1
 	google.golang.org/grpc v1.63.0
 	google.golang.org/protobuf v1.33.0
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"net"
 
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/micro-tok/video-service/pkg/cassandra"
 	"github.com/micro-tok/video-service/pkg/config"
 	"github.com/micro-tok/video-service/pkg/pb"
+	"github.com/micro-tok/video-service/pkg/redis"
 	"github.com/micro-tok/video-service/pkg/s3"
 	"github.com/micro-tok/video-service/pkg/services"
 	"google.golang.org/grpc"
@@ -18,15 +20,19 @@ func main() {
 
 	s3Service := s3.NewAWSService(cfg)
 
+	redisService := redis.NewRedisService(cfg)
+
 	lis, err := net.Listen("tcp", "localhost:"+cfg.ServicePort)
 	if err != nil {
 		panic(err)
 	}
 
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(
+		grpc.MaxRecvMsgSize(1024 * 1024 * 1024 * 2),
+	)
 
 	// Register the service
-	pb.RegisterVideoServiceServer(grpcServer, services.NewVideoService(cassService, s3Service))
+	pb.RegisterVideoServiceServer(grpcServer, services.NewVideoService(cassService, s3Service, redisService))
 
 	if err := grpcServer.Serve(lis); err != nil {
 		panic(err)
